8fe6e18e94fbead3568ce8787b26b0ab183d2051
questionMarcEksdi <69903079+questionMarcEksdi@users.noreply.github.com>
2022-02-16 16:39:00 +0800

    Delete dist directory

--- dist/index.fbb7b822.js
+++
@@ -1,8 +0,0 @@
-// changing the banner when the breakpoint hits 768 pixels
-window.addEventListener('resize', changeBanner);
-function changeBanner() {
-    if (window.innerWidth > 768) document.querySelector('.banner__img').src = "/assets/images/02_05_PBC_WebBanner.jpg";
-    else document.querySelector('.banner__img').src = "/assets/images/mobile/02_05_PBC_WebBanner_Mobile.jpg";
-}
-
-//# sourceMappingURL=index.fbb7b822.js.map
